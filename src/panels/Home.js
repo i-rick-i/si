3a47c28551aa3a7e62291621c45e3ac1a5b81a53
i-rick-i <83608226+i-rick-i@users.noreply.github.com>
2021-08-29 00:54:40 +0300

    q

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar} from '@vkontakte/vkui';
 
-import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar } from '@vkontakte/vkui';
-
-const Home = ({ id, go, fetchedUser }) => (
+const Home=({id, go, fetchedUser})=>(
 	<Panel id={id}>
-		<PanelHeader>Example</PanelHeader>
+		<PanelHeader>Нелло,</PanelHeader>
 		{fetchedUser &&
-		<Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
-			<Cell
-				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
-			>
-				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-			</Cell>
-		</Group>}
+			<Group header={<Header mode="secondary">Похоже, что это ты</Header>}>
+				<Cell before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null} description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}>
+					{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
+				</Cell>
+			</Group>
+		}
 
-		<Group header={<Header mode="secondary">Navigation Example</Header>}>
+		<Group header={<Header mode="secondary">Кнопочки</Header>}>
 			<Div>
 				<Button stretched size="l" mode="secondary" onClick={go} data-to="persik">
-					Show me the Persik, please
+					Показать перскиа?
 				</Button>
 			</Div>
 		</Group>
--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import { Panel, PanelHeader, PanelHeaderBack } from '@vkontakte/vkui';
-
+import {Panel, PanelHeader, PanelHeaderBack, PromoBanner} from '@vkontakte/vkui';
 import persik from '../img/persik.png';
 import './Persik.css';
 
-const Persik = props => (
+const promoBannerProps={
+  title: 'Заголовок',
+  domain: 'vk.com',
+  trackingLink: 'https://vk.com',
+  ctaText: 'Перейти',
+  advertisingLabel: 'Реклама',
+  iconLink: 'https://sun9-7.userapi.com/c846420/v846420985/1526c3/ISX7VF8NjZk.jpg',
+  description: 'Описание рекламы',
+  ageRestrictions: "14+",
+  statistics: [
+    {url: '', type: 'playbackStarted'},
+    {url: '', type: 'click'}
+  ]
+};
+
+const Persik=props=>(
 	<Panel id={props.id}>
-		<PanelHeader
-			left={<PanelHeaderBack onClick={props.go} data-to="home"/>}
-		>
-			Persik
+		<PanelHeader left={<PanelHeaderBack onClick={props.go} data-to="home"/>}>
+			А это Персик]
 		</PanelHeader>
 		<img className="Persik" src={persik} alt="Persik The Cat"/>
+    
 	</Panel>
 );
 
-Persik.propTypes = {
+Persik.propTypes={
 	id: PropTypes.string.isRequired,
 	go: PropTypes.func.isRequired,
 };
